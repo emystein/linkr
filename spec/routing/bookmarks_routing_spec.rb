b5ac6d88bfe5eeb4605ea6c52bae2d892930e7bc
Emiliano Menéndez <emystein@gmail.com>
2019-02-28 22:04:33 -0300

    Fix routing tests

--- spec/routing/bookmarks_routing_spec.rb
+++ spec/routing/bookmarks_routing_spec.rb
@@ -1,34 +1,34 @@
 require "spec_helper"
 
-describe BookmarksController do
+describe BookmarksController, :type => :routing do
   describe "routing" do
 
     it "routes to #index" do
-      get("/bookmarks").should route_to("bookmarks#index")
+      expect(get("/bookmarks")).to route_to("bookmarks#index")
     end
 
     it "routes to #new" do
-      get("/bookmarks/new").should route_to("bookmarks#new")
+      expect(get("/bookmarks/new")).to route_to("bookmarks#new")
     end
 
     it "routes to #show" do
-      get("/bookmarks/1").should route_to("bookmarks#show", :id => "1")
+      expect(get("/bookmarks/1")).to route_to("bookmarks#show", :id => "1")
     end
 
     it "routes to #edit" do
-      get("/bookmarks/1/edit").should route_to("bookmarks#edit", :id => "1")
+      expect(get("/bookmarks/1/edit")).to route_to("bookmarks#edit", :id => "1")
     end
 
     it "routes to #create" do
-      post("/bookmarks").should route_to("bookmarks#create")
+      expect(post("/bookmarks")).to route_to("bookmarks#create")
     end
 
     it "routes to #update" do
-      put("/bookmarks/1").should route_to("bookmarks#update", :id => "1")
+      expect(put("/bookmarks/1")).to route_to("bookmarks#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/bookmarks/1").should route_to("bookmarks#destroy", :id => "1")
+      expect(delete("/bookmarks/1")).to route_to("bookmarks#destroy", :id => "1")
     end
 
   end
