393a17ee2d547fc5ea8c20ffc10547f644ed7f63
Emiliano Menéndez <emystein@gmail.com>
2019-03-02 00:57:18 -0300

    WIP: removed unneede user controller create method replaced by devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,17 +7,4 @@ class UsersController < ApplicationController
     @user = User.find_by_id(params[:id])
     @bookmarks = @user.bookmarks.public_bookmarks.paginate(:page => params[:page])
   end
-
-  def create  
-    @user = User.new(user_params)
-    if @user.save  
-      redirect_to login_path, :flash => {:success => "Signed up successfully!"}
-    else  
-      render "new"  
-    end  
-  end
-
-  def user_params
-    params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :salt, :encrypted_password)
-  end
 end
