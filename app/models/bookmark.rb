899ff8752b1d9e428ffeeb3b64693c57d3782b88
Emiliano Menéndez <emystein@gmail.com>
2019-09-24 23:55:29 -0300

    Cleaned up yabs csv import

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -4,12 +4,12 @@ class Bookmark < ActiveRecord::Base
   acts_as_taggable_on :tags
 
   default_scope { order(created_at: :desc) }
-  scope :public_bookmarks, -> { where(:private => false) }
+  scope :public_bookmarks, -> { where(private: false) }
 
   belongs_to :location
   belongs_to :user
 
-  delegate :url, :to => :location, :allow_nil => true
+  delegate :url, to: :location, allow_nil: true
 
   validates_presence_of :title
 
@@ -18,12 +18,12 @@ class Bookmark < ActiveRecord::Base
   end
 
   def find_or_generate_location
-    self.location = Location.find_or_create_by(:url => @url)
+    self.location = Location.find_or_create_by(url: @url)
   end
 
   # see also: https://www.justinweiss.com/articles/search-and-filter-rails-models-without-bloating-your-controller/
   def self.search(query)
-    self.where("lower(title) like lower(?)", "%#{query}%")
+    where('lower(title) like lower(?)', "%#{query}%")
   end
 
   require 'smarter_csv'
@@ -33,18 +33,13 @@ class Bookmark < ActiveRecord::Base
 
     rows = SmarterCSV.process(file.path)
 
-    # Skip row with summary 'Generated on:...'
     rows = rows.filter { |row| row[:id].is_a? Integer }
 
     transaction do
       rows.each do |row|
-        if !Location.where(:url => row[:link]).exists? then
-          bookmark = Bookmark.new(user: user, title: row[:title], url: row[:link], private: row[:state] != 'public')
-          # bookmark = Bookmark.where(location.url: row[:link]).first_or_create(user: user, title: row[:title], url: row[:link], private: row[:state] != 'public')
-          bookmark.tag_list.add(row[:tags], parse: true)
-          bookmark.save
-          bookmarks << bookmark
-        end
+        next if exist_location_with_url(row[:link])
+
+        bookmarks << save_bookmark_from_csv_row(row, user)
       end
     end
 
@@ -53,5 +48,16 @@ class Bookmark < ActiveRecord::Base
     # bookmarks = Bookmark.where(:id => bookmark_ids).paginate(:page => params[:page])
 
     bookmarks
+  end
+
+  def self.exist_location_with_url(url)
+    Location.where(url: url).exists?
+  end
+
+  def self.save_bookmark_from_csv_row(row, user)
+    bookmark = Bookmark.new(user: user, title: row[:title], url: row[:link], private: row[:state] != 'public')
+    bookmark.tag_list.add(row[:tags], parse: true)
+    bookmark.save
+    bookmark
   end
 end
