5278ab1131f2cebc6bc4311facb62f01b364474d
Emiliano Menéndez <emystein@gmail.com>
2019-10-26 20:45:22 -0300

    Subset of tag count by date spec

--- spec/models/tag_count_by_date_spec.rb
+++ spec/models/tag_count_by_date_spec.rb
@@ -21,10 +21,21 @@ describe TagCountByDate, type: :model do
     expect(tag_count_by_date[2].name).to eq 'tag3'
   end
 
-  it 'take a subset of tags' do
-    tag_count_by_date = TagCountByDate.all.take(2)
+  describe 'Subset' do
+    it 'take less than the total of tags' do
+      tag_count_by_date = TagCountByDate.all.take(2)
 
-    expect(tag_count_by_date[0].name).to eq 'tag1'
-    expect(tag_count_by_date[1].name).to eq 'tag2'
+      expect(tag_count_by_date[0].name).to eq 'tag1'
+      expect(tag_count_by_date[1].name).to eq 'tag2'
+    end
+
+    it 'take more than the total of tags' do
+      tag_count_by_date = TagCountByDate.all.take(4)
+
+      expect(tag_count_by_date.count).to eq 3
+      expect(tag_count_by_date[0].name).to eq 'tag1'
+      expect(tag_count_by_date[1].name).to eq 'tag2'
+      expect(tag_count_by_date[2].name).to eq 'tag3'
+    end
   end
 end
