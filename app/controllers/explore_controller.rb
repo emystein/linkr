f47d85859b487ed9024fb276ef103c3561651bf0
Emiliano Menéndez <emystein@gmail.com>
2019-10-26 21:05:14 -0300

    This Week Trending Tags in explore page

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,6 +1,6 @@
 class ExploreController < ApplicationController
   def index
     @bookmarks = Bookmark.public_bookmarks.take(10)
-    @tag_count_by_date = TagCountByDate.all
+    @tag_count_by_date = TagCountByDate.where('created_at >= ?', 1.week.ago).take(30)
   end
 end
--- spec/models/tag_count_by_date_spec.rb
+++ spec/models/tag_count_by_date_spec.rb
@@ -16,26 +16,26 @@ describe TagCountByDate, type: :model do
   it 'tags are sorted by date and count' do
     tag_count_by_date = TagCountByDate.all
 
-    expect(tag_count_by_date[0].name).to eq 'tag1'
-    expect(tag_count_by_date[1].name).to eq 'tag2'
-    expect(tag_count_by_date[2].name).to eq 'tag3'
+    expect(tag_count_by_date.map{|t| t.name}).to eq ['tag1', 'tag2', 'tag3']
+  end
+
+  it 'retrieve last week' do
+    tag_count_by_date = TagCountByDate.all.where('created_at >= ?', 1.week.ago)
+
+    expect(tag_count_by_date.count).to eq 3
   end
 
   describe 'Subset' do
     it 'take less than the total of tags' do
       tag_count_by_date = TagCountByDate.all.take(2)
 
-      expect(tag_count_by_date[0].name).to eq 'tag1'
-      expect(tag_count_by_date[1].name).to eq 'tag2'
+      expect(tag_count_by_date.map{|t| t.name}).to eq ['tag1', 'tag2']
     end
 
     it 'take more than the total of tags' do
       tag_count_by_date = TagCountByDate.all.take(4)
 
-      expect(tag_count_by_date.count).to eq 3
-      expect(tag_count_by_date[0].name).to eq 'tag1'
-      expect(tag_count_by_date[1].name).to eq 'tag2'
-      expect(tag_count_by_date[2].name).to eq 'tag3'
+      expect(tag_count_by_date.map{|t| t.name}).to eq ['tag1', 'tag2', 'tag3']
     end
   end
 end
