395497c30aba676ebe9fe48c9560f2b4fc0d559b
Emiliano Menéndez <emystein@gmail.com>
2019-10-31 23:57:53 -0300

    Cleaned up BundledTag spec

--- spec/models/bundled_tag_spec.rb
+++ spec/models/bundled_tag_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe BundledTag, type: :model do
   before do
     @user = create(:user)
+    @tag = create(:tag)
   end
 
   it 'creates a BundledTag' do
-    tag1 = Tag.create(name: 'tag1')
-    tag2 = Tag.create(name: 'tag2')
-
     tag_bundle = TagBundle.create(user: @user, name: 'tag_bundle1')
 
-    bundle_tag = BundledTag.create(tag_bundle: tag_bundle, tag: tag1)
+    bundle_tag = BundledTag.create(tag_bundle: tag_bundle, tag: @tag)
+
+    expect(BundledTag.find(bundle_tag.id)).to be_present
   end
 end
