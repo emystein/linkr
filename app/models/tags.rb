a401fdd640c93c12ecccdcad87bd2a345256fb8f
Emiliano Menéndez <emystein@gmail.com>
2019-11-06 18:28:17 -0300

    Tags factory filter out empty input strings

--- app/models/tags.rb
+++ app/models/tags.rb
@@ -1,6 +1,6 @@
 class Tags
   def self.from_names(joined_tag_names)
-    tag_names = joined_tag_names.split(',').map{ |n| n.strip}
+    tag_names = joined_tag_names.split(',').map{ |n| n.strip}.filter{ |n| !n.empty? }
     tag_names.map {|name| Tag.find_or_create_by(name: name) }
   end
 end
--- spec/models/tags_spec.rb
+++ spec/models/tags_spec.rb
@@ -7,4 +7,20 @@ RSpec.describe Tags, type: :model do
     expect(tags[0].name).to eq 'tag1'
     expect(tags[1].name).to eq 'tag2'
   end
+  it 'empty input dont create Tags' do
+    tags = Tags.from_names('')
+    expect(tags).to be_empty
+  end
+  it 'only one comma input dont create Tags' do
+    tags = Tags.from_names(',')
+    expect(tags).to be_empty
+  end
+  it 'only commas input dont create Tags' do
+    tags = Tags.from_names(',,')
+    expect(tags).to be_empty
+  end
+  it 'only spaces separated by commas input dont create Tags' do
+    tags = Tags.from_names('  ,  ,  ')
+    expect(tags).to be_empty
+  end
 end
