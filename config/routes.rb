e140ef04727c9eb95cd4e917c44f5110c08e3c12
Emiliano Menéndez <emystein@gmail.com>
2020-04-11 23:21:44 -0300

    Cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'explore/index'
-  devise_for :user, path_names: { sign_up: "signup", sign_in: "login", sign_out: "logout" }
-
+  devise_for :user
+  
   devise_scope :user do
     post "/sessions/user", to: "devise/sessions#create" # devise post to this url on login
   end
-
+  
+  get "/explore"                      => "explore#index"
   get "/help"                         => "pages#help"
   get "/dashboard"                    => "dashboard#show"
   get "/dashboard/tags/:tag"          => "dashboard#tag"
@@ -17,8 +17,10 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :sessions, :only => [:new, :create, :destroy]
+  # resources :sessions, :only => [:new, :create, :destroy]
+
   resources :tags, :only => [:index, :show]
+  
   resources :tag_bundles
 
   resources :bookmarks do
